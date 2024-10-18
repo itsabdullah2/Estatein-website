a0ec781b12f04ab1b7dbc254b061f37ba31cb2c9
itsabdullah2 <abdullahosman3020@gmail.com>
2024-10-18 16:31:41 +0300

    display the children component to their parent

--- src/components/aboutUs/AboutUs.tsx
+++ src/components/aboutUs/AboutUs.tsx
@@ -1,5 +1,21 @@
+import OurJourney from "./aboutUsComponents/OurJourney";
+import OurValues from "./aboutUsComponents/OurValues";
+import OurAchievements from "./aboutUsComponents/OurAchievements";
+import NavigatingExperience from "./aboutUsComponents/NavigatingExperience";
+import MeetOurTeam from "./aboutUsComponents/MeetOurTeam";
+import OurClients from "./aboutUsComponents/OurClients";
+
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <div className="px-4 sm:px-10 md:px-20 2xl:px-[10.125rem] customSize:px-20">
+      <OurJourney />
+      <OurValues />
+      <OurAchievements />
+      <NavigatingExperience />
+      <MeetOurTeam />
+      <OurClients />
+    </div>
+  );
 };
 
 export default AboutUs;
