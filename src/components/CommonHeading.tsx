523d47c65ee335911ae465c0af299d5a333ad081
itsabdullah2 <abdullahosman3020@gmail.com>
2024-10-21 22:13:43 +0300

    initial commit for reusable component

---
+++ src/components/CommonHeading.tsx
@@ -0,0 +1,27 @@
+import { headingStars } from "../assets/dummy";
+
+interface Props {
+  title: string;
+  description: string;
+  className?: string;
+}
+
+const CommonHeading = ({ className, title, description }: Props) => {
+  return (
+    <div className={`${className}`}>
+      <img
+        src={headingStars}
+        alt="Heading Stars"
+        className="w-[68.4px] h-[30px] -translate-x-[10px]"
+      />
+      <h2 className="text-[28px] lg:text-5xl font-semibold text-white mt-[10px] mb-[14px]">
+        {title}
+      </h2>
+      <p className="text-gray60 text-[14px] lg:text-[18px] font-medium">
+        {description}
+      </p>
+    </div>
+  );
+};
+
+export default CommonHeading;
