3f2819ecafbb40909509a8eb4bd8587c35639626
itsabdullah2 <abdullahosman3020@gmail.com>
2024-10-23 19:13:42 +0300

    add the sections that I finished

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -1,5 +1,21 @@
+import ElevateYourExperience from "./servicesComponents/ElevateYourExperience";
+import Encourage from "./servicesComponents/encourage";
+import UnlockProperty from "./servicesComponents/UnlockProperty";
+import EffortlessManagement from "./servicesComponents/EffortlessManagement";
+import SmartInvestment from "./servicesComponents/SmartInvestment";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <>
+      <ElevateYourExperience />
+      <Encourage />
+      <div className="px-4 sm:px-10 md:px-20 2xl:px-[10.125rem] customSize:px-20">
+        <UnlockProperty />
+        <EffortlessManagement />
+        <SmartInvestment />
+      </div>
+    </>
+  );
 };
 
 export default Services;
