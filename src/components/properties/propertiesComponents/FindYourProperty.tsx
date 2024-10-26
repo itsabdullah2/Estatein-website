8a8b78cc2e7944fa6dc39b224bd56c91d62f368c
itsabdullah2 <abdullahosman3020@gmail.com>
2024-10-27 02:51:42 +0300

    add id to the parent div

--- src/components/properties/propertiesComponents/FindYourProperty.tsx
+++ src/components/properties/propertiesComponents/FindYourProperty.tsx
@@ -3,7 +3,10 @@ const findYourPropertyDescription: string =
 
 const FindYourProperty = () => {
   return (
-    <div className="pl-4 pt-[50px] pr-4 pb-[50px] md:pl-20 md:pt-[100px] md:pb-[130px] md:pr-[200px] xl:pl-40 xl:pt-[150px] xl:pb-40 xl:pr-[400px] bg-[linear-gradient(120deg,#262626_-25%,rgba(0,0,0,0.00)_30%)] border-b border-gray15">
+    <div
+      id="find-property"
+      className="pl-4 pt-[50px] pr-4 pb-[50px] md:pl-20 md:pt-[100px] md:pb-[130px] md:pr-[200px] xl:pl-40 xl:pt-[150px] xl:pb-40 xl:pr-[400px] bg-[linear-gradient(120deg,#262626_-25%,rgba(0,0,0,0.00)_30%)] border-b border-gray15"
+    >
       <div className="flex flex-col gap-[10px] xl:gap-[14px]">
         <h2 className="text-[28px] md:text-[36px] xl:text-5xl font-semibold leading-[150%] text-white">
           Find Your Dream Property
